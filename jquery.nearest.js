e038d66c1464af4818786872b3d75c948ed77850
Jenna Smith <jenna@growldesign.co.uk>
2012-11-09 11:13:35 +0000

    adding nearest plugin

---
+++ jquery.nearest.js
@@ -0,0 +1,23 @@
+/*
+ * jQuery nearest
+ * Example at: http://jsbin.com/ucavon/3/edit
+ * Copyright (c) 2012 J. Smith (@jjenzz)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ * $('.foo').nearest('.bar');
+ * $('.foo').nearest('p');
+ */
+$.fn.nearest = function(selector) {
+  var nearest,
+      parent = this[0] ? this[0].parentNode : 0,
+      hasQsa = parent && parent.querySelectorAll;
+  while (parent) {
+    nearest = hasQsa ?
+      parent.querySelectorAll(selector) : $(parent).find(selector);
+    if (nearest.length) break;
+    parent = parent.parentNode;
+  }
+  return $(nearest);
+};
